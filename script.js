0eebc45943ea8d5710caec516739834e4a57bea6
Rebecca Tay <rebeccatay92@gmail.com>
2017-06-22 23:08:11 +0800

    check this version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var randomNumber = randomFn(0,10);
-var form = document.getElementById('form');
+var randomNumber = randomFn(0, 10)
+var form = document.getElementById('form')
 
 // fn that generates randomNum
 // min and max always whole number
@@ -9,7 +9,7 @@ function randomFn (min, max) {
 }
 
 // this fn. updates h1 text content
-function updateH1(newH1) {
+function updateH1 (newH1) {
   var h1 = document.querySelector('h1')
   // var h1 = document.getElementsByTagName('h1')
   // updated the property of h1 dom node
@@ -17,29 +17,28 @@ function updateH1(newH1) {
 }
 
 document.querySelector('h4').addEventListener('click', function () {
-  document.querySelector('h4').style.display = 'none';
-  startNewGame();
+  document.querySelector('h4').style.display = 'none'
+  startNewGame()
 })
 
-function startNewGameButton(){
-  document.querySelector('h4').style.display = 'block';
+function startNewGameButton () {
+  document.querySelector('h4').style.display = 'block'
 }
 
-function startNewGame(){
-  randomNumber = randomFn(0,10);
-  document.querySelector('h1').textContent = 'Higher Or Lower Game!';
+function startNewGame () {
+  randomNumber = randomFn(0, 10)
+  document.querySelector('h1').textContent = 'Higher Or Lower Game!'
 }
 
-//using the guess button
+// using the guess button
 document.querySelector('h2').addEventListener('click', function () {
-  var number = document.getElementById("number").value;
+  var number = document.getElementById('number').value
   askForANumber(number, randomNumber)
 })
 
-
-//using the enter in the form. this changes html but doesnt stay
+// using the enter in the form. this changes html but doesnt stay
 form.onsubmit = function () {
-  var number = document.getElementById('number').value;
+  var number = document.getElementById('number').value
   askForANumber(number, randomNumber)
 }
 
@@ -51,9 +50,9 @@ function askForANumber (guessedNum, randomNumber) {
   } else if (guessedNum < randomNumber) {
     updateH1('Go higher')
     document.querySelector('body').style.backgroundColor = 'rgb(119, 230, 233)'
-  } else if(guessedNum == randomNumber) {
+  } else if (guessedNum == randomNumber) {
     updateH1('Correct')
     document.querySelector('body').style.backgroundColor = 'green'
-    startNewGameButton();
+    startNewGameButton()
   }
 }
